3bc246139350f81cb34f968efb99bf1066e09159
Jiahui Ji <61010678+Jiahui-Ji@users.noreply.github.com>
2021-12-07 16:03:58 +0000

    Add files via upload

---
+++ code_cell/4_sam_to_fastq.sh
@@ -0,0 +1,59 @@
+perl sam_to_fastq_v2.pl CBTM-361B-AX-cells-D1.sam
+perl sam_to_fastq_v2.pl CBTM-364B-AX-cells-D3.sam
+perl sam_to_fastq_v2.pl CBTM-364B-LV-cells-D3.sam
+perl sam_to_fastq_v2.pl CBTM-364B-LA-cells-D3.sam
+perl sam_to_fastq_v2.pl CBTM-364B-SP-cells-D3.sam
+perl sam_to_fastq_v2.pl CBTM-364B-RV-cells-D3.sam
+
+perl sam_to_fastq_v2.pl CBTM-386C-RV-cells-D4.sam
+perl sam_to_fastq_v2.pl CBTM-386C-RA-cells-D4.sam
+perl sam_to_fastq_v2.pl CBTM-386C-AX-cells-D4.sam
+perl sam_to_fastq_v2.pl CBTM-386C-LV-cells-D4.sam
+perl sam_to_fastq_v2.pl CBTM-386C-LA-cells-D4.sam
+perl sam_to_fastq_v2.pl CBTM-386C-SP-cells-D4.sam
+
+perl sam_to_fastq_v2.pl CBTM-390C-RA-cells-D5.sam
+perl sam_to_fastq_v2.pl CBTM-390C-LV-cells-D5.sam
+perl sam_to_fastq_v2.pl CBTM-390C-SP-cells-D5.sam
+perl sam_to_fastq_v2.pl CBTM-390C-RV-cells-D5.sam
+perl sam_to_fastq_v2.pl CBTM-390C-AX-cells-D5.sam
+perl sam_to_fastq_v2.pl CBTM-390C-LA-cells-D5.sam
+
+perl sam_to_fastq_v2.pl CBTM-417C-AX-cd45-D6.sam
+perl sam_to_fastq_v2.pl CBTM-417C-SP-cells-D6.sam
+perl sam_to_fastq_v2.pl CBTM-417C-RV-cd45-D6.sam
+perl sam_to_fastq_v2.pl CBTM-417C-LV-cd45-D6.sam
+perl sam_to_fastq_v2.pl CBTM-417C-RA-cd45-D6.sam
+perl sam_to_fastq_v2.pl CBTM-417C-LA-cells-D6.sam
+perl sam_to_fastq_v2.pl CBTM-417C-LV-cells-D6.sam
+perl sam_to_fastq_v2.pl CBTM-417C-LA-cd45-D6.sam
+perl sam_to_fastq_v2.pl CBTM-417C-RA-cells-D6.sam
+perl sam_to_fastq_v2.pl CBTM-417C-RV-cells-D6.sam
+perl sam_to_fastq_v2.pl CBTM-417C-AX-cells-D6.sam
+perl sam_to_fastq_v2.pl CBTM-417C-SP-cd45-D6.sam
+
+perl sam_to_fastq_v2.pl CBTM-423C-RA-cd45-D7.sam
+perl sam_to_fastq_v2.pl CBTM-423C-LV-cd45-D7.sam
+perl sam_to_fastq_v2.pl CBTM-423C-LV-cells-D7.sam
+perl sam_to_fastq_v2.pl CBTM-423C-LA-cells-D7.sam
+perl sam_to_fastq_v2.pl CBTM-423C-RA-cells-D7.sam
+perl sam_to_fastq_v2.pl CBTM-423C-SP-cells-D7.sam
+perl sam_to_fastq_v2.pl CBTM-423C-LA-cd45-D7.sam
+perl sam_to_fastq_v2.pl CBTM-423C-SP-cd45-D7.sam
+perl sam_to_fastq_v2.pl CBTM-423C-RV-cells-D7.sam
+perl sam_to_fastq_v2.pl CBTM-423C-AX-cells-D7.sam
+perl sam_to_fastq_v2.pl CBTM-423C-AX-cd45-D7.sam
+perl sam_to_fastq_v2.pl CBTM-423C-RV-cd45-D7.sam
+
+perl sam_to_fastq_v2.pl CBTM-473C-LV-cells-D11.sam
+perl sam_to_fastq_v2.pl CBTM-473C-RV-cells-D11.sam
+perl sam_to_fastq_v2.pl CBTM-473C-AX-cells-D11.sam
+perl sam_to_fastq_v2.pl CBTM-473C-SP-cd45-D11.sam
+perl sam_to_fastq_v2.pl CBTM-473C-RA-cells-D11.sam
+perl sam_to_fastq_v2.pl CBTM-473C-LA-cells-D11.sam
+perl sam_to_fastq_v2.pl CBTM-473C-SP-cells-D11.sam
+perl sam_to_fastq_v2.pl CBTM-473C-AX-cd45-D11.sam
+perl sam_to_fastq_v2.pl CBTM-473C-LA-cd45-D11.sam
+perl sam_to_fastq_v2.pl CBTM-473C-RV-cd45-D11.sam
+perl sam_to_fastq_v2.pl CBTM-473C-RA-cd45-D11.sam
+perl sam_to_fastq_v2.pl CBTM-473C-LV-cd45-D11.sam
---
+++ code_cell/6_cellranger_map.sh
@@ -0,0 +1,113 @@
+S=CBTM-361B-AX-cells-D1
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-364B-AX-cells-D3
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-364B-LV-cells-D3
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-364B-LA-cells-D3
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-364B-SP-cells-D3
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-364B-RV-cells-D3
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+
+S=CBTM-386C-RV-cells-D4
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-386C-RA-cells-D4
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-386C-AX-cells-D4
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-386C-LV-cells-D4
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-386C-LA-cells-D4
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-386C-SP-cells-D4
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+
+S=CBTM-390C-RA-cells-D5
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-390C-LV-cells-D5
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-390C-SP-cells-D5
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-390C-RV-cells-D5
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-390C-AX-cells-D5
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-390C-LA-cells-D5
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+
+S=CBTM-417C-AX-cd45-D6
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-417C-SP-cells-D6
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-417C-RV-cd45-D6
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-417C-LV-cd45-D6
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-417C-RA-cd45-D6
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-417C-LA-cells-D6
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-417C-LV-cells-D6
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-417C-LA-cd45-D6
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-417C-RA-cells-D6
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-417C-RV-cells-D6
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-417C-AX-cells-D6
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-417C-SP-cd45-D6
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+
+S=CBTM-423C-RA-cd45-D7
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-423C-LV-cd45-D7
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-423C-LV-cells-D7
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-423C-LA-cells-D7
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-423C-RA-cells-D7
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-423C-SP-cells-D7
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-423C-LA-cd45-D7
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-423C-SP-cd45-D7
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-423C-RV-cells-D7
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-423C-AX-cells-D7
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-423C-AX-cd45-D7
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-423C-RV-cd45-D7
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+
+S=CBTM-473C-LV-cells-D11
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-473C-RV-cells-D11
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-473C-AX-cells-D11
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-473C-SP-cd45-D11
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-473C-RA-cells-D11
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-473C-LA-cells-D11
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-473C-SP-cells-D11
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-473C-AX-cd45-D11
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-473C-LA-cd45-D11
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-473C-RV-cd45-D11
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-473C-RA-cd45-D11
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
+S=CBTM-473C-LV-cd45-D11
+cellranger count --id=$S --fastqs=$FQ --sample=$S --transcriptome=$GENOME --localcores=10 --include-introns
